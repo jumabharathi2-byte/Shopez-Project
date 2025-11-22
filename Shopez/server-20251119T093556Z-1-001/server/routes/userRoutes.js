29f3f372510f3a4c54457fb5acd9556b10c69875
jumabharathi2-byte <jumabharathi2@gmail.com>
2025-11-22 11:28:42 +0530

    Add files via upload

---
+++ Shopez/server-20251119T093556Z-1-001/server/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { registerUser, loginUser, fetchUsers } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+// Public routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/all', fetchUsers);  // <-- Added public route
+
+// Protected route
+router.get('/fetch-users', protect, fetchUsers);
+
+export default router;
